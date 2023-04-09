e49d3051119dc91997ed323186e163c696ae2f11
AndriySafonov <andreygold18@gmail.com>
2023-04-09 22:22:31 +0300

    added coments

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -5,28 +5,44 @@ import { IoSearchSharp } from 'react-icons/io5';
 import css from './Searchbar.module.css';
 
 export function Searchbar(props) {
+  // useState и useRef хуки для хранения и обновления 
+  // значения текстового поля
   const [inputValue, setInputValue] = useState('');
   const inputRef = useRef(inputValue);
 
+
+  // функция handleChange, вызывается при изменении 
+  // значения текстового поля и обновляет значение 
+  // inputValue.
   function handleChange(event) {
     setInputValue(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    const trimmedInputValue = inputValue.trim();
-    if (trimmedInputValue === '') {
+     // Проверяем значения текстового поля.  Если 
+    // значение пустое, отображается уведомление 
+    // об ошибке 
+    if (inputValue.trim() === '') {
       Notify.warning('Opss...try again');
       return;
     }
-    props.onSubmit(trimmedInputValue);
+    // Если значение не пустое, вызывается функция 
+    // onSubmit, передавая ей обрезанное значение 
+    // текстового поля, и обновляется значение inputValue.
+    props.onSubmit(inputValue.trim());
     setInputValue('');
   }
 
+  // Используем useEffect хук для обновления значения 
+  // inputRef при изменении значения inputValue.
   useEffect(() => {
     inputRef.current = inputValue;
   }, [inputValue]);
 
+  // Используем useEffect хук для получения значения 
+  // inputRef при первом рендере компонента и 
+  // обновления значения inputValue, если значение inputRef не пустое.
   useEffect(() => {
     const savedInputValue = inputRef.current;
     if (savedInputValue) {

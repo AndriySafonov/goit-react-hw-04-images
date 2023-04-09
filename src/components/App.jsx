825a66e2aaf10fc61cc6c2fa591be834de38137c
AndriySafonov <andreygold18@gmail.com>
2023-04-09 21:21:22 +0300

    refactored the code in the App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
@@ -7,113 +7,98 @@ import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    page: 1,
-    totalHits: 0,
-    imageCards: [],
-    loading: false,
-    showModal: false,
-    selectedImgCard: null,
-  };
+export function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [imageCards, setImageCards] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImgCard, setSelectedImgCard] = useState(null);
 
-  // метод обрабатывает изменения searchQuery и page, 
-  // и отправляет запрос на сервер с помощью FetchApi 
-  // для получения картинок. Если запрос не удался, 
-  // то он уведомляет пользователя об ошибке с помощью
-  //  Notify.   
-  componentDidUpdate(_, prevState) {
-    if (
-      this.state.searchQuery !== prevState.searchQuery ||
-      this.state.page !== prevState.page
-    ) {
-      const { searchQuery, page } = this.state;
-
-      this.setState({ loading: true, totalHits: 0 });
+  //  хук используется для отправки запроса на сервер с помощью 
+  // FetchApi и обработки результата.Если запрос не удался, то 
+  // уведомляет пользователя об ошибке с помощью Notify.
+  useEffect(() => {
+    if (searchQuery !== '' && page > 0) {
+      setLoading(true);
+      setTotalHits(0);
 
       const fetchResponse = FetchApi(searchQuery, page);
       fetchResponse
         .then(resp => {
-          console.log(resp)
+          console.log(resp);
           if (resp.data.hits.length === 0) {
-            this.setState({ loading: false });
+            setLoading(false);
             Notify.warning('Oops! Find better)');
             return;
           }
 
-          this.setState(() => ({
-            imageCards: [...this.state.imageCards, ...resp.data.hits],
-            totalHits: resp.data.totalHits,
-          }));
+          setImageCards(prevImageCards => [
+            ...prevImageCards,
+            ...resp.data.hits,
+          ]);
+          setTotalHits(resp.data.totalHits);
 
           if (resp.data.total === 0) {
-            Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+            Notify.warning(
+              'Sorry, there are no images matching your search query. Please try again.'
+            );
           }
         })
         .catch(error => {
           console.log(error);
           Notify.failure('Something went wrong...');
-          this.setState({ loading: false });
         })
-        .finally(() => this.setState({ loading: false }));
+        .finally(() => setLoading(false));
     }
-  }
+  }, [searchQuery, page]);
 
-  // onSubmit()вызываем при отправке формы поиска. Он устанавливает 
+  // onSubmit()вызываем при отправке формы поиска. Он устанавливает
   // новое значение для searchQuery, если оно отличается от предыдущего значения.
-  onSubmit = inputValue => {
-    if (this.state.searchQuery !== inputValue) {
-      this.setState({ searchQuery: inputValue, imageCards: [], page: 1 });
+  const onSubmit = inputValue => {
+    if (searchQuery !== inputValue) {
+      setSearchQuery(inputValue);
+      setImageCards([]);
+      setPage(1);
     }
   };
 
-  // при нажатии кнопки увеличиваем значение page на 1
-  onLoadBtnClick = () => {
-    this.setState(({ page }) => ({
-      page: page + 1,
-    }));
+  // при нажатии кнопки "Load more...", увеличиваем значение page на 1
+  const onLoadBtnClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
   // переключает значение showModal между true и false
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
   };
 
-  // вызываем при клике на картинку,  устанавливаем ее как 
+  // вызываем при клике на картинку,  устанавливаем ее как
   // selectedImgCard и отображаем в модальном окне
-  onImgClick = imgId => {
-    const imageCard = this.state.imageCards.find(
-      imageCard => imageCard.id === imgId
-    );
+  const onImgClick = imgId => {
+    const imageCard = imageCards.find(imageCard => imageCard.id === imgId);
 
-    this.setState({ selectedImgCard: imageCard, showModal: true });
+    setSelectedImgCard(imageCard);
+    setShowModal(true);
   };
+  return (
+    <div>
+      <Searchbar onSubmit={onSubmit} />
+      {/* проверяем, есть ли imageCards, и если да, то отображаем галерею */}
+      {imageCards.length > 0 && (
+        <ImageGallery imageCardsArray={imageCards} onImgClick={onImgClick} />
+      )}
 
-  render() {
-    const { imageCards, loading, totalHits, showModal, selectedImgCard, page } =
-      this.state;
+      {showModal && (
+        <Modal onClose={toggleModal} selectedImgCard={selectedImgCard} />
+      )}
 
-    return (
-      <div >
-        <Searchbar onSubmit={this.onSubmit} />
+      {/*  Если loading true, то отображаем индикатор загрузки Loader */}
+      {loading && <Loader />}
 
-        {/* проверяем, есть ли imageCards, и если да, то отображаем галерею */}
-        {imageCards.length > 0 && (
-          <ImageGallery imageCardsArray={imageCards} onImgClick={this.onImgClick} />
-        )}
-
-        {showModal && (
-          <Modal onClose={this.toggleModal} selectedImgCard={selectedImgCard} />
-        )}
-
-        {/*  Если loading true, то отображаем индикатор загрузки Loader */}
-        {loading && <Loader />}
-
-        {/* если картинок пришло больше 12, отображается кнопка Load more... */}
-        {(page * 12 <= totalHits) && <Button onClick={this.onLoadBtnClick} />}
-
-      </div>
-    );
-  }
+      {/* если картинок пришло больше 12, отображается кнопка "Load more..." */}
+      {page * 12 <= totalHits && <Button onClick={onLoadBtnClick} />}
+    </div>
+  );
 }
